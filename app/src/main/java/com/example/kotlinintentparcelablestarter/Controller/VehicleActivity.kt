bf923409c60bd0d68793e8bd6b9b4dd431357862
Ahil <eahilendran@gmail.com>
2019-02-20 03:03:14 +0530

    PaintColorActivity Designed and implement Button toggling

--- app/src/main/java/com/example/kotlinintentparcelablestarter/Controller/VehicleActivity.kt
+++ app/src/main/java/com/example/kotlinintentparcelablestarter/Controller/VehicleActivity.kt
@@ -1,13 +1,50 @@
 package com.example.kotlinintentparcelablestarter.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.kotlinintentparcelablestarter.R
+import kotlinx.android.synthetic.main.activity_vehicle.*
 
 class VehicleActivity : AppCompatActivity() {
 
+    var selectedVehicle = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicle)
     }
+
+    fun vehicleNextBtnClicked(view: View){
+        if(selectedVehicle != ""){
+            val paintIntent = Intent(this,PaintColorActivity::class.java)
+            startActivity(paintIntent)
+        }
+        else{
+            Toast.makeText(this,"Please select vehicle",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun bikeBtnClicked(view: View){
+        carBtn.isChecked = false
+        jeepBtn.isChecked = false
+        selectedVehicle = "bike"
+    }
+    fun carBtnClicked(view: View){
+        bikeBtn.isChecked = false
+        jeepBtn.isChecked = false
+        selectedVehicle = "car"
+
+    }
+    fun jeepBtnClicked(view: View){
+        bikeBtn.isChecked = false
+        carBtn.isChecked = false
+        selectedVehicle = "jeep"
+
+    }
+
 }
+//
+//
+//
--- app/src/main/java/com/example/kotlinintentparcelablestarter/Controller/WelcomeActivity.kt
+++ app/src/main/java/com/example/kotlinintentparcelablestarter/Controller/WelcomeActivity.kt
@@ -1,13 +1,20 @@
 package com.example.kotlinintentparcelablestarter.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinintentparcelablestarter.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        goGarageBtn.setOnClickListener {
+            val vehicleIntent = Intent(this,VehicleActivity::class.java)
+            startActivity(vehicleIntent)
+        }
     }
 }
