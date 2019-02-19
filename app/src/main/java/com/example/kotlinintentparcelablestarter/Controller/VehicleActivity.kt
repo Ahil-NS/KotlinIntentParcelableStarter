c6d645cd6d0ec96d0845f614c0800f591cf47573
Ahil <eahilendran@gmail.com>
2019-02-19 23:34:41 +0530

    Welcome, Vehicle Activity UI Designed

---
+++ app/src/main/java/com/example/kotlinintentparcelablestarter/Controller/VehicleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinintentparcelablestarter.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinintentparcelablestarter.R
+
+class VehicleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle)
+    }
+}
